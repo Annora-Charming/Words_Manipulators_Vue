9500be7191e7d6227c0c17902813f5b1f646a938
harchebnikovaay <harchebnikova99@mail.ru>
2021-09-28 10:11:56 +0700

    Wrote tests and the component itself
    Написала тесты для компонента StringReverser и сам компонент
    
    Wrote tests for the StringReverser component and wrote the component itself

--- src/components/ReverseString.vue
+++ src/components/ReverseString.vue
@@ -1,12 +1,27 @@
 <template>
   <div>
     <h2>String Reverser</h2>
+    <input id="reverserInput" v-model="value" />
+    <div id="buttons">
+      <button id="meow" v-on:click="value = value + '(=`ω´=)'">meow</button>
+    </div>
+    {{ reversedString }}
   </div>
 </template>
 
 <script>
 export default {
   name: "ReverseString",
+  data() {
+    return {
+      value: "",
+    };
+  },
+  computed: {
+    reversedString() {
+      return this.value.split("").reverse().join("");
+    },
+  },
 };
 </script>
 
