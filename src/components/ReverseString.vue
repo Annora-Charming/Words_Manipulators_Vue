27f493e5f12a8a8e66f8e80291d918a31a4ac69a
harchebnikovaay <harchebnikova99@mail.ru>
2021-09-28 10:32:07 +0700

    Style changes
    Изменила css для компонента и немного для всего приложения, добавила кнопки в компонент
    
    Changed css for component and also for the app a little bit. Added buttons into the component

--- src/components/ReverseString.vue
+++ src/components/ReverseString.vue
@@ -1,9 +1,15 @@
 <template>
-  <div>
+  <div id="reverseString">
     <h2>String Reverser</h2>
     <input id="reverserInput" v-model="value" />
     <div id="buttons">
       <button id="meow" v-on:click="value = value + '(=`ω´=)'">meow</button>
+      <button v-on:click="value = value + 'U・ᴥ・U'">woof</button>
+      <button v-on:click="value = value + 'ʕಠᴥಠʔ'">roar</button>
+      <button v-on:click="value = value + '／(˃ᆺ˂)＼'">bun bun</button>
+      <button v-on:click="value = value + '(◉Θ◉)'">hoot hoot</button>
+      <button v-on:click="value = 'ERROR'">ERROR</button>
+      <button v-on:click="value = ''">Clear all</button>
     </div>
     {{ reversedString }}
   </div>
@@ -25,4 +31,34 @@ export default {
 };
 </script>
 
-<style scoped lang="sass"></style>
+<style scoped lang="sass">
+#reverseString
+  padding: 10px
+  margin: 10px
+  border: 2px dashed #5a2002
+  border-radius: 20px
+  display: flex
+  flex-direction: column
+  align-items: center
+  font: 30px "Monospaced"
+
+#buttons
+  width: 700px
+  display: flex
+  align-items: center
+  justify-content: space-between
+
+button
+  font-size: 20px
+  width: auto
+
+#reverserInput
+  font: 30px "Monospaced"
+  color: antiquewhite
+  width: 500px
+  background-color: black
+  border-radius: 20px
+  margin: 10px
+  padding: 5px
+  text-align: center
+</style>
