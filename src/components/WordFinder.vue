62b06d616108ab1ec6baebd167be612112aab62b
harchebnikovaay <harchebnikova99@mail.ru>
2021-10-05 10:34:06 +0700

    Started WordFinder and its tests

---
+++ src/components/WordFinder.vue
@@ -0,0 +1,11 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "WordFinder",
+};
+</script>
+
+<style scoped></style>
---
+++ tests/unit/WordFinder.spec.js
@@ -0,0 +1,8 @@
+// import WordFinder from "../../src/components/WordFinder";
+// import { shallowMount } from "@vue/test-utils";
+
+describe("", () => {
+  it("", () => {});
+  it("", () => {});
+  it("", () => {});
+});
