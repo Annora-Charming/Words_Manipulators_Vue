ba4c05633c3c980550cd794590654e0653eb7569
harchebnikovaay <harchebnikova99@mail.ru>
2021-10-05 14:10:40 +0700

    Wrote WordFinder component and its tests
    Написала тесты к компоненту WordFinder и сам компонент. Есть обращение к серверу, а сам сервер лежит в репе words
    
    Wrote tests for the component and the component itself. There is fetching and server as ib the "words" repository

--- src/components/WordFinder.vue
+++ src/components/WordFinder.vue
@@ -1,10 +1,45 @@
 <template>
-  <div></div>
+  <div id="wordFinder">
+    <h2>Word Finder</h2>
+    <input
+      id="wordFinderInput"
+      placeholder="Start entering here"
+      v-model="value"
+      v-on:keypress.enter="getWords()"
+    />
+    <button id="wordsFinderButton" v-on:click="getWords">get</button>
+    {{ filtrate }}
+  </div>
 </template>
 
 <script>
 export default {
   name: "WordFinder",
+  data() {
+    return {
+      value: "",
+      words: [],
+    };
+  },
+  methods: {
+    async getWords() {
+      const response = await fetch("http://localhost:3001/?q=" + this.value);
+      this.words = await response.json();
+    },
+  },
+  computed: {
+    filtrate() {
+      let filtratedWords = [];
+      for (
+        let i = 0;
+        i < this.words.length && filtratedWords.length < 10;
+        i++
+      ) {
+        filtratedWords.push(this.words[i]);
+      }
+      return filtratedWords;
+    },
+  },
 };
 </script>
 
